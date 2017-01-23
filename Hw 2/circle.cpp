9992d1d09a5e1d7273143183757e7bc9f9dac251
Michael Hsu <michael100.hsu@gmail.com>
2017-01-23 15:48:28 -0600

    edited circle

--- Hw 2/circle.cpp
+++ Hw 2/circle.cpp
@@ -25,10 +25,7 @@ bool overlapped(Circle C1, Circle C2)
   double bothRadii = C1.radius + C2.radius;
   double distance = sqrt(pow((C2.x-C1.x),2) + pow((C2.y-C1.y),2));
   
-  if(distance < bothRadii)
-    return true;
-  else
-    return false;
+  return distance < bothRadii;
 }
 
 int main() {
@@ -38,18 +35,16 @@ int main() {
 
   // Infinite loop until the last value is negative
   while(true) {
-    if(cin.get() == '\n') {
-      Circle cNext = read_circle();
+    Circle cNext = read_circle();
 
-      if(cNext.radius < 0)
-        break;
+    if(cNext.radius < 0)
+      break;
       
-      // Check if circles are overlapping
-      if(!overlapped(c1, cNext))
-	output = output + "not overlapped\n";
-      else
-	output = output + "overlapped\n";
-    }
+    // Check if circles are overlapping
+    if(!overlapped(c1, cNext))
+      output = output + "not overlapped\n";
+    else
+      output = output + "overlapped\n";
   }
     
   cout << output << '\n';
